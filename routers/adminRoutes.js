cb9b5073cbb2a65a72bd2e8cdac38d8fccc70e91
Prateek579 <prateekmaurya9236@gmail.com>
2024-05-14 09:20:12 +0530

    adding automatic rendering functionality

--- routers/adminRoutes.js
+++ routers/adminRoutes.js
@@ -13,37 +13,40 @@ const problem = require("../model/query");
 
 const { transporter } = require("../helpers/emailHelpers");
 
-const ad = Admin({username:"sahilkaitha@gmail.com",password:"123",fullName:"Sahil Hossain",Mobile:"9635955320"})
-ad.save()
-
+const ad = Admin({
+  username: "sahilkaitha@gmail.com",
+  password: "123",
+  fullName: "Sahil Hossain",
+  Mobile: "9635955320",
+});
+ad.save();
 
 router.post("/admin-login", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
+  const admin = await Admin.findOne({ username: username, password: password });
+  console.log(admin);
+  try {
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
 
-        const admin = await Admin.findOne({ username: username, password: password });
-            console.log(admin);
-        try {
-          const dooner = await User.find(); // Assuming User is your Mongoose model for users
-          const ngo=await NGO.find()
-    
-          const query1 = await problem.find();
-          res.render("Admin_Dashboard", {
-              name: admin.fullName,
-              email: admin.fullName,
-              mobile:admin.Mobile,
-              username:admin.username,
-              id: admin._id,
-              NGOname: ngo,
-              Donername: dooner,
-              UserName: "sahil114",
-              complain:query1
-          });
-      } catch (err) {
-          console.error(err);
-          res.status(500).send("An internal server error occurred.");
-      }
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An internal server error occurred.");
+  }
 });
 
 router.get("/admin-logout", function (req, res) {
@@ -66,36 +69,54 @@ router.get("/admin-dashboard", async function (req, res) {
   }
 });
 
-router.post("/approve-ngo/:id", async (req, res) => {
+router.post("/approve-ngo/:id/:userId", async (req, res) => {
+  console.log("approve ngo call");
   const ngoId = req.params.id;
-
+  const userId = req.params.userId;
   try {
-    const ngo = await NGO.findById(ngoId);
-    if (!ngo) {
+    const ngo1 = await NGO.findById(ngoId);
+    if (!ngo1) {
       return res.status(404).json({ error: "NGO not found" });
     }
 
     // Update the NGO's approval status
 
-    ngo.approved = true;
-    await ngo.save();
+    ngo1.approved = true;
+    await ngo1.save();
 
     // Send an email to the NGO with the approved details
-    let mailOptions = {
-      to: ngo.username,
-      subject: "NGO Registration Approved",
-      text: "Your NGO registration has been approved. You can now login to your account.",
-      // Include any necessary information in the email body
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+    // let mailOptions = {
+    //   to: ngo.username,
+    //   subject: "NGO Registration Approved",
+    //   text: "Your NGO registration has been approved. You can now login to your account.",
+    //   // Include any necessary information in the email body
+    // };
+    // transporter.sendMail(mailOptions, function (error, info) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log("Email sent: " + info.response);
+    //   }
+    // });
 
-    res.status(200).json({ message: "NGO approved successfully" });
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     console.error("Error approving NGO:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -103,18 +124,36 @@ router.post("/approve-ngo/:id", async (req, res) => {
 });
 
 //making POST request for decline ngo
-router.post("/decline-ngo/:id", async (req, res) => {
+router.post("/decline-ngo/:id/:userId", async (req, res) => {
   const ngoId = req.params.id;
+  const userId = req.params.userId;
   try {
     const ngo1 = await NGO.findById(ngoId);
     if (!ngo1) {
       return res.status(404).json({ error: "NGO not found" });
     }
 
-    //deleting the NGO from data base
+    //DELETING NGO from database
     await NGO.findByIdAndDelete(ngoId);
 
-    res.status(200).json({ message: "Ngo decline successfully" });
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     console.error("Error declining NGO:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -122,8 +161,9 @@ router.post("/decline-ngo/:id", async (req, res) => {
 });
 
 // making POST method for declining the DONOR request
-router.post("/decline-donor/:id", async (req, res) => {
+router.post("/decline-donor/:id/:userId", async (req, res) => {
   const donorId = req.params.id;
+  const userId = req.params.userId;
   try {
     const donor = await User.findById(donorId);
     if (!donor) {
@@ -132,7 +172,25 @@ router.post("/decline-donor/:id", async (req, res) => {
 
     //DELETING donor request from data base
     await User.findByIdAndDelete(donorId);
-    res.status(200).json({ message: "doner declined successfully" });
+
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     console.error("Error declining doner:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -140,8 +198,9 @@ router.post("/decline-donor/:id", async (req, res) => {
 });
 
 // making POST method for acceptiog the DONOR request
-router.post("/accept-donor/:id", async (req, res) => {
+router.post("/accept-donor/:id/:userId", async (req, res) => {
   const donorId = req.params.id;
+  const userId = req.params.userId;
   try {
     const donor = await User.findById(donorId);
     if (!donor) {
@@ -152,7 +211,24 @@ router.post("/accept-donor/:id", async (req, res) => {
     donor.approved = true;
     await donor.save();
 
-    res.status(200).json({ message: "Doner accepted successfully" });
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     console.error("Error declining doner:", error);
     res.status(500).json({ error: "Internal server error" });
@@ -160,8 +236,9 @@ router.post("/accept-donor/:id", async (req, res) => {
 });
 
 //making DELETE method for DELETING the complaints
-router.post("/delete-complain/:id", async (req, res) => {
+router.post("/delete-complain/:id/:userId", async (req, res) => {
   const compId = req.params.id;
+  const userId = req.params.userId;
   const comp = await problem.findById(compId);
   if (!comp) {
     return res.status(404).json({ error: "Complain not found" });
@@ -170,17 +247,33 @@ router.post("/delete-complain/:id", async (req, res) => {
     //DELETING the complain from data base
     await problem.findByIdAndDelete(compId);
 
-    res
-      .status(200)
-      .json({ message: "Complain deleted successfully successfully" });
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     res.status(500).jso({ error: "Internal server error" });
   }
 });
 
 //making POST method for accepting the complaints
-router.post("/accept-complain/:id", async (req, res) => {
+router.post("/accept-complain/:id/:userId", async (req, res) => {
   const compId = req.params.id;
+  const userId = req.params.userId;
   const comp = await problem.findById(compId);
   if (!comp) {
     return res.status(404).json({ error: "Complain not found" });
@@ -190,7 +283,24 @@ router.post("/accept-complain/:id", async (req, res) => {
     comp.approved = true;
     await comp.save();
 
-    res.status(200).json({ message: "Complain approved successfully" });
+    // navigating to the admin dashboard
+    const admin = await Admin.findById(userId);
+    console.log("admin details in approve-ngo", admin);
+    const dooner = await User.find(); // Assuming User is your Mongoose model for users
+    const ngo = await NGO.find();
+
+    const query1 = await problem.find();
+    res.render("Admin_Dashboard", {
+      name: admin.fullName,
+      email: admin.fullName,
+      mobile: admin.Mobile,
+      username: admin.username,
+      id: admin._id,
+      NGOname: ngo,
+      Donername: dooner,
+      UserName: "sahil114",
+      complain: query1,
+    });
   } catch (error) {
     res.status(500).jso({ error: "Internal server error" });
   }
